1a33ea897845f9d6c958435bb4337c0b6c9323ce
Pratham Shroff <pshroff@bu.edu>
2022-11-08 15:02:41 -0500

    app is done!
    SharedPreferences created, onDestroy() & onPause() made

--- app/src/main/java/com/example/w6_p2/MainActivity.java
+++ app/src/main/java/com/example/w6_p2/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +34,25 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
 
+
+
+        if (sharedPreferences.contains("imagePreference")) {
+            String imageString = sharedPreferences.getString("imagePreference", "");
+            byte[] imageBytes = Base64.decode(imageString, Base64.DEFAULT);
+            Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            image.setImageBitmap(decodedImage);
+        }
+        if (sharedPreferences.contains("editText")) {
+            String str = sharedPreferences.getString("editText", "");
+            editText.setText(str);
+        }
+
+        SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+        preferencesEditor.clear();
+        preferencesEditor.apply();
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,5 +65,46 @@ public class MainActivity extends AppCompatActivity {
                 image.setImageResource(resID);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,bos);
+        byte[] bb = bos.toByteArray();
+        String imageString = Base64.encodeToString(bb,Base64.DEFAULT);
+
+        myEdit.putString("imagePreference", imageString);
+        myEdit.putString("editText", editText.getText().toString());
+        myEdit.apply();
+
+        // resources used for help:
+        // https://stackoverflow.com/questions/7315498/how-to-store-and-retrieve-bitmap-in-sharedpreferences-in-android
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,bos);
+        byte[] bb = bos.toByteArray();
+        String imageString = Base64.encodeToString(bb,Base64.DEFAULT);
+
+        myEdit.putString("imagePreference", imageString);
+        myEdit.putString("editText", editText.getText().toString());
+        myEdit.apply();
     }
 }
